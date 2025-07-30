cd2ad61f45880b576d51828f176a2e330b55dde4
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-07-30 16:53:12 +0530

    git initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to MindSketch</h1>
+      <p>Your one-stop solution for all your mind mapping needs.</p>
+    </div>
+  )
+}
+
+export default App
